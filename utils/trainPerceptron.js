f28463732ebc43ca2236ac5c552f6edeba9da870
Artem <melnykartem10@gmail.com>
2018-03-11 16:40:25 +0100

    Added main function

---
+++ utils/trainPerceptron.js
@@ -0,0 +1,19 @@
+const fs=require('fs');
+
+let getGeuss=(w,x,n)=>{
+    
+}
+
+let correctGeuss=(w,x,n)=>{
+
+}
+
+let getNewWeights=(w,x,n)=>{
+
+}
+
+let trainPerceptron=(w,inputs,n)=>{
+
+}
+
+module.exports={trainPerceptron,getGeuss}
